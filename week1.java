bf5ccf2d94b179d9f6869d367b5cb6f5169ac0c5
pushpinder00002222 <166749538+pushpinder00002222@users.noreply.github.com>
2024-04-11 14:17:08 -0400

    Add files via upload

---
+++ week1.java
@@ -0,0 +1,12 @@
+package com.example.week1;
+
+public class week1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Welcome to week 01 \n");
+		System.out.println ("We are learning Java✅!");
+		System.out.print("It's an online class");
+	}
+ 
+}
